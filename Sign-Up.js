9e6f4353c3a45bc9cb4fa1f8f3b257146ad238d8
hwblue24 <125501572+hwblue24@users.noreply.github.com>
2024-05-23 16:48:22 -0400

    Set up form validation for all input fields
    Pass confirmed separately future will use JS to
    update cross check between fields

---
+++ Sign-Up.js
@@ -0,0 +1,10 @@
+let password = document.getElementById('Password'); 
+
+let confirmPassword = document.getElementById('Confirm_Password');
+
+function myPasswordCheck (password,confirmPassword) { 
+    if (password === confirmPassword) { 
+        console.log('good')
+
+    }
+}
